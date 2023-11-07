9f6590c0baa8b7c899c2f63ccc6ffe852450a48d
Adil Usta <adilusta4@gmail.com>
2023-11-07 23:19:55 +0300

    BusinessLayer tamamlandı.

---
+++ SignalR.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using SignalR.EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Abstract
+{
+    public interface IGenericService<TEntity> where TEntity : class,IEntity,new()
+    {
+        void TAdd(TEntity entity);
+        void TDelete(TEntity entity);
+        void TUpdate(TEntity entity);
+        TEntity TGetById(int id);
+        List<TEntity> TGetListAll();
+    }
+}
