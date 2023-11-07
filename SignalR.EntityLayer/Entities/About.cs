7ecbefcafff2b2f26a743b296ffe038bcebbe367
Adil Usta <adilusta4@gmail.com>
2023-11-07 04:26:58 +0300

    Entity sınıfları eklendi

---
+++ SignalR.EntityLayer/Entities/About.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string ImageURL { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
