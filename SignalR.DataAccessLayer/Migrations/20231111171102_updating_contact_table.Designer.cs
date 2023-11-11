130f69513e4dccadbeffc440310ab188e86451c5
Adil Usta <adilusta4@gmail.com>
2023-11-11 20:19:08 +0300

    Contactapi yazıldı.Contactdto lar güncellendi.

--- SignalR.DataAccessLayer/Migrations/20231111171102_updating_contact_table.Designer.cs
+++ SignalR.DataAccessLayer/Migrations/20231111171102_updating_contact_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalR.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using SignalR.DataAccessLayer.Concrete;
 namespace SignalR.DataAccessLayer.Migrations
 {
     [DbContext(typeof(SignalRDbContext))]
-    partial class SignalRDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231111171102_updating_contact_table")]
+    partial class updating_contact_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
