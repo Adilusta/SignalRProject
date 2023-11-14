9e75c01c610413c50f18a3b686744c23941652a2
Adil Usta <adilusta4@gmail.com>
2023-11-14 21:53:26 +0300

    Category ve Product tablosu arasında ilişki kuruldu.

--- SignalR.DataAccessLayer/Migrations/20231111181723_mig_add_relation_product_category.Designer.cs
+++ SignalR.DataAccessLayer/Migrations/20231111181723_mig_add_relation_product_category.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalR.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using SignalR.DataAccessLayer.Concrete;
 namespace SignalR.DataAccessLayer.Migrations
 {
     [DbContext(typeof(SignalRDbContext))]
-    partial class SignalRDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231111181723_mig_add_relation_product_category")]
+    partial class mig_add_relation_product_category
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SignalR.EntityLayer/Entities/Category.cs
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -7,6 +7,7 @@ namespace SignalR.EntityLayer.Entities
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
 
     }
 }
