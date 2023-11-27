3fb63e47c1ba19cd15abc0f690e1802785e638b9
Adil Usta <adilusta4@gmail.com>
2023-11-27 03:14:31 +0300

    Admin tarafında testimonial crud işlemleri yapıldı.

--- SignalRWebUI/Dtos/TestimonialDtos/CreateTestimonialDto.cs
+++ SignalRWebUI/Dtos/TestimonialDtos/CreateTestimonialDto.cs
@@ -11,7 +11,7 @@ namespace SignalRWebUI.Dtos.TestimonialDtos
         public string Name { get; set; }
         public string Title { get; set; }
         public string Comment { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageURL { get; set; }
         public bool Status { get; set; }
     }
 }
--- SignalRWebUI/Dtos/TestimonialDtos/UpdateTestimonialDto.cs
+++ SignalRWebUI/Dtos/TestimonialDtos/UpdateTestimonialDto.cs
@@ -12,7 +12,7 @@ namespace SignalRWebUI.Dtos.TestimonialDtos
         public string Name { get; set; }
         public string Title { get; set; }
         public string Comment { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageURL { get; set; }
         public bool Status { get; set; }
     }
 }
