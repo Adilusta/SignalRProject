b2ead200e8b3be33a0481ce1c485d3ee51202eb8
Adil Usta <adilusta4@gmail.com>
2024-02-03 00:17:00 +0300

    Menü sayfası oluşturuldu ve ürünler getirildi.

---
+++ SignalRWebUI/ViewComponents/MenuComponents/MenuNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.MenuComponents
+{
+    public class MenuNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
