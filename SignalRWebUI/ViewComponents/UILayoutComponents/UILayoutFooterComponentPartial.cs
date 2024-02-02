190f530bcf38b31b95d979fddfeb514a7dcb7e49
Adil Usta <adilusta4@gmail.com>
2024-02-02 23:34:46 +0300

    Footer kısmı dinamik hale getirildi.

--- SignalRWebUI/ViewComponents/UILayoutComponents/UILayoutFooterComponentPartial.cs
+++ SignalRWebUI/ViewComponents/UILayoutComponents/UILayoutFooterComponentPartial.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ContactDtos;
 
 namespace SignalRWebUI.ViewComponents.UILayoutComponents
 {
 	public class UILayoutFooterComponentPartial: ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        private readonly IHttpClientFactory _httpClientFactory;
+        public UILayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7053/api/Contact");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return View(values);
+        }
+    }
 }
