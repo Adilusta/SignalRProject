a58024a83b4109ddef6bc85a6159f8a3a7b37a1f
Adil Usta <adilusta4@gmail.com>
2024-02-02 22:59:04 +0300

    Referanslar(bizim için ne dediler?) kısmı yapıldı.

--- SignalRWebUI/ViewComponents/DefaultComponents/DefaultAboutComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/DefaultAboutComponentPartial.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.AboutDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
 	public class DefaultAboutComponentPartial: ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultAboutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7053/api/About");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+            return View(values);
+        }
+    }
 }
--- SignalRWebUI/ViewComponents/DefaultComponents/DefaultTestimonialComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/DefaultTestimonialComponentPartial.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.TestimonialDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class DefaultTestimonialComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7053/api/Testimonial");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+            return View(values);
         }
     }
 }
