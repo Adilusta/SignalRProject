1a88c3ce04852abd47a07026fd90d3c5c80a2244
Adil Usta <adilusta4@gmail.com>
2024-02-02 22:34:40 +0300

    Öne çıkan ürünler kısmı yapıldı.

--- SignalRWebUI/ViewComponents/DefaultComponents/DefaultOurMenuComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/DefaultOurMenuComponentPartial.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
 	public class DefaultOurMenuComponentPartial: ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultOurMenuComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7053/api/Product");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+        }
+    }
 }
